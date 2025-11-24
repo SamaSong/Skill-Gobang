e711fbc0b57f866ec4fdc1bb1cb3d529d0597864
Gao Song <liuxz@dachkj.com>
2025-11-24 15:48:50 +0800

    feat:
    开发

---
+++ src/components/Chessboard.vue
@@ -0,0 +1,11 @@
+<teleport>
+
+</teleport>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/view/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+ import ChessBoard from '../components/Chessboard.vue'
+</script>
+
+<template>
+  <ChessBoard></ChessBoard>
+</template>
+
+<style scoped lang="scss">
+
+</style>
